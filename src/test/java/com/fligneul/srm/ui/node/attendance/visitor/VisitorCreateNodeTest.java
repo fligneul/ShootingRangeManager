774e4fbacd8780aec72e8a2adda8f27f4ed9dd37
Florian L <florian.ligneul@gmail.com>
2022-12-06 01:35:50 +0100

    Feature #5 : Add visitor
        - Test to fix unit tests

--- src/test/java/com/fligneul/srm/ui/node/attendance/visitor/VisitorCreateNodeTest.java
+++ src/test/java/com/fligneul/srm/ui/node/attendance/visitor/VisitorCreateNodeTest.java
@@ -51,7 +51,7 @@ class VisitorCreateNodeTest {
         fxRobot.clickOn(visitorCreateNode.lastnameTextField).write("TEST_LASTNAME");
         FxAssert.verifyThat(visitorCreateNode.lastnameTextField, PseudoClassMatchers.withoutPseudoClass("error"));
 
-        fxRobot.clickOn(visitorCreateNode.dateOfBirthPicker).write("01/01/2000");
+        fxRobot.clickOn(visitorCreateNode.dateOfBirthPicker.getEditor()).write("01/01/2000");
         FxAssert.verifyThat(visitorCreateNode.dateOfBirthPicker, PseudoClassMatchers.withoutPseudoClass("error"));
     }
 }
